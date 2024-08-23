95fa83fb2248749aa98d19ffdfe75817b9765887
jorge <jluiz@morphus.com.br>
2024-08-23 12:45:11 -0300

    ajuste

--- scripts-by-module/module-1.sh
+++ scripts-by-module/module-1.sh
@@ -4,7 +4,7 @@ echo "***************************************************"
 echo "--- This could take around 20 minutes"
 
 # Create the Kubernetes Cluster
-    eksctl create cluster -f Infraestrutura/eksctl/01-initial-cluster/cluster.yaml
+    eksctl create cluster -f Infraestrutura/eksctl/01-Inicial-cluster/cluster.yaml
 
 echo "*************************************************************"
 echo "********* READY TO CHAPTER 2! - FINISHED AT $(date) *********"
